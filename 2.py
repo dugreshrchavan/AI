46333458c4e7bd7313d0ab7887ce35ec8d994eb6
durgeshrchavan <74813731+dugreshrchavan@users.noreply.github.com>
2023-12-14 21:59:24 +0530

    Update 2

--- 2.py
+++ 2.py
@@ -1,58 +1,28 @@
-class Graph:
-    def __init__(self):
-        self.graph = {}
-
-    def add_edge(self, node, neighbor):
-        if node not in self.graph:
-            self.graph[node] = []
-        self.graph[node].append(neighbor)
-
-def dfs(graph, start, goal, visited=None, path=None):
-    if visited is None:
-        visited = set()
-    if path is None:
-        path = []
-
-    visited.add(start)
-    path = path + [start]
-
-    if start == goal:
-        return path
-
-    if start not in graph:
-        return None
-
-    for neighbor in graph[start]:
-        if neighbor not in visited:
-            new_path = dfs(graph, neighbor, goal, visited, path)
-            if new_path:
-                return new_path
-
-    return None
-
-if __name__ == "__main__":
-    # Create the graph
-    my_graph = Graph()
-    my_graph.add_edge(1, 2)
-    my_graph.add_edge(1, 3)
-    my_graph.add_edge(2, 4)
-    my_graph.add_edge(2, 5)
-    my_graph.add_edge(3, 6)
-    my_graph.add_edge(3, 7)
-    my_graph.add_edge(4, 8)
-    my_graph.add_edge(5, 8)
-    my_graph.add_edge(6, 8)
-    my_graph.add_edge(7, 8)
-
-    # Set initial and goal nodes
-    initial_node = 1
-    goal_node = 8
-
-    # Find path using DFS
-    result_path = dfs(my_graph.graph, initial_node, goal_node)
-
-    # Print the result
-    if result_path:
-        print(f"Path from {initial_node} to {goal_node}: {result_path}")
-    else:
-        print(f"No path found from {initial_node} to {goal_node}")
+SLIP1.
+Q2
+1. Model the following Property system as a document database. Consider a
+set of Property, Owner. One owner can buy many properties
+>>>create database=durgesh
+>use durgesh
+2. Assume appropriate attributes and collections as per the query requirements.
+>>db.createCollection(“owner”);
+>>>db.createCollection(“propertys”);
+>>show collection
+3.Insert at least 05 documents in each collection.
+>>db.product.insertMany([{“......”}]);
+>>db.owner.insertMany([{.....”}]);
+4. Answer the following Queries
+a. Display area wise property details.
+>>db.properties1.find({},{area:1,_id:101});
+b. Display property owned by 'Mr.Patil' having minimum rate
+>>db.Property.find({ ownerId: db.Owner.findOne({ name: "Mr. Patil" })._id }).sort({
+. rate: 1 }).limit(1)
+///this Query not correct— tula jml tr mala pn takkk………
+c. Give the details of owner whose property is at “Nashik”.
+>>db.owner.findOne({ "propertiesowned": { $in: [db.Property.findOne({ area: "Nashik"
+. })]}});
+………… OR……..
+db.properties1.find({area:"Nashik"})
+d. Display area of property whose rate is less than 100000.
+>>db.properties1.find( { rate: { $gt:(100000)} } );
+Q1
